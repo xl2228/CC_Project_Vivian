7b7a7cd8b9a402082d533a0ba57936bb87f454af
xl2228 <42978279+xl2228@users.noreply.github.com>
2020-03-03 12:20:23 -0500

    Update mySketch.js
    Developing scenes

--- sketch850621/mySketch.js
+++ sketch850621/mySketch.js
@@ -1,19 +1,25 @@
 var penguin;
+var scene=0;
 let snowflakes = [];
+
 function setup() {
 	createCanvas(800, 800);
 	background(168, 209, 255);
-	penguin = new Penguin();
-
+	penguin = new Penguin(200,700,10,0,0,0);
+	penguinMom = new Penguin(650,700,10,1,95,235);
+	
+	penguinA = new Penguin(200,700,10,0,0,0);
+	penguinAplus = new Penguin(650,700,10,1,95,235);		
 }
 
 function draw() {
 	console.log(frameCount);
+	                   //****SCENE1****
 	background(168, 209, 255);
 	noStroke();
 	if (frameCount <=300){
-	//snow on the ground
 	frameRate (40);
+	//snow on the ground
 	drawSnow (0,0);
 	drawSnow (150,10);
 	drawSnow (300,-10);
@@ -37,9 +43,19 @@ function draw() {
   }
 	
 	//tremble the penguin
-	penguin.tremble(198,202,698,702);
-	}
-else if (frameCount >300 && frameCount <= 800){
+	penguin.tremble();
+		
+		
+		           //****SCENE2****
+	}else if (frameCount >300 && frameCount <= 700){
+	//snow on the ground
+	drawSnow (0,0);
+	drawSnow (150,10);
+	drawSnow (300,-10);
+	drawSnow (400,0);
+	drawSnow (500,15);
+	drawSnow (680,7);
+	
 	//falling snowflake
 	let t = frameCount / 60;
   for (let j = 0; j < random(5); j++) {
@@ -49,12 +65,23 @@ else if (frameCount >300 && frameCount <= 800){
     flake.update(t); 
     flake.display(); 
   }
-	penguin.display();
-	penguin.move();
-}
-else if (frameCount >800 && frameCount<=1200){
-	translate(400,600);
-	penguin.display();
+	
+	//penguin moving toward her mom
+	let s= frameCount*0.8;
+	penguin.display(s);
+	
+	penguin.move(s);
+
+	
+	//tremble the penguin
+	penguin.tremble();
+	
+	//penguin mom
+	penguinMom.display();
+             	//****SCENE3****
+}else if (frameCount >700 && frameCount<=1100){
+	penguinA.display();
+	penguinAplus.display();
 }
 	
 	
@@ -72,25 +99,34 @@ function mousePressed(){
   print(mouseY);
 }
 */
-function Penguin(){
-	this.x = 200;
-	this.y = 700;
-	this.dist = 10;
+
+//Penguin
+function Penguin(xPos, yPos,distance,r_,g_,b_){
+	this.x = xPos;
+	this.y = yPos;
+	this.dist = distance;
+	this.r = r_;
+	this.g = g_;
+	this.b = b_;
 	
 	this.display = function(){
 	push();
-	fill(0);
+	fill(this.r,this.g,this.b);
 	noStroke();
-	ellipse(this.x,this.y,80,130);
+	//body and hands
+	ellipse(this.x,this.y,8*this.dist,13*this.dist);
+	ellipse(this.x-4*this.dist,this.y-this.dist,25,13);
+	ellipse(this.x+4*this.dist,this.y-this.dist,25,13);
 	fill(255);
-	ellipse(this.x,this.y+8,60,90);
+	//belly
+	ellipse(this.x,this.y+8,6*this.dist,9*this.dist);
+	//white of the eye
 	ellipse(this.x-this.dist,this.y-4*this.dist,2*this.dist,2*this.dist);
 	ellipse(this.x+9,this.y-4*this.dist,2*this.dist,2*this.dist);
 	fill(0);
 	ellipse(this.x-this.dist,this.y-4*this.dist,8,8);
 	ellipse(this.x+9,this.y-4*this.dist,8,8);
-	ellipse(this.x-4*this.dist,this.y-this.dist,25,13);
-	ellipse(this.x+4*this.dist,this.y-this.dist,25,13);
+	
 	fill(255,192,112);
 	triangle(this.x-8,this.y-30,this.x+7,this.y-30,this.x,this.y-20);
 	ellipse(this.x-16,this.y+64,25,13);
@@ -99,13 +135,13 @@ function Penguin(){
 		
 		}
 		
-	this.tremble = function(upX,downX,upY,downY){	
-			this.x = random(upX,downX);
-			this.y = random(upY,downY);
+	this.tremble = function(){	
+			this.x = random(this.x-2,this.x+2);
+			this.y = random(this.y-2,this.y+2);
 	}
-	this.move = function(){
-			this.x = random(frameCount*0.5,frameCount);
-			this.y = random(frameCount*0.5,frameCount);
+	this.move = function(s){
+			this.x = s;
+			this.y = 700;
 			
 	}
 }
@@ -116,6 +152,11 @@ function drawSnow(snowX,snowY){
 	noStroke();
 	ellipse(90,780,200,110);
 	pop();
+}
+function Fireplace(){
+	
+	
+	
 }
 
 //snowflake code from the example https://p5js.org/examples/simulate-snowflakes.html
@@ -141,4 +182,6 @@ function snowflake() {
     fill(255);
     ellipse(this.posX, this.posY, this.size);
   };
+	
+	
 }
